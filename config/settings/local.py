3e9b75931d1bd957fb0e7e3270585a0f6e2aad12
pidokige02 <pidokige0204@gmail.com>
2023-04-30 17:11:49 +0900

    css access error fix

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,5 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = ['165.232.117.29']
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
+ALLOWED_HOSTS = ['165.232.117.29','127.0.0.1']
+# STATIC_ROOT = BASE_DIR / 'static/'
+# STATICFILES_DIRS = []
